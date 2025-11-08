64db0fed1327e3080d495d1e78611c8a6ba02bb3
AmiraAHady <amirasgithub@gmail.com>
2025-11-08 12:15:31 +0200

    edit strings

--- strings.py
+++ strings.py
@@ -10,3 +10,22 @@ print(message)  # Output: Hello, Amira!
 age = 25
 intro = f"My name is {name}, and I’m {age} years old."
 print(intro)
+
+# 2. String formatting
+age = 25
+intro = f"My name is {name}, and I’m {age} years old."
+print(intro)
+
+# 3. Change case
+print(name.upper())   # AMIRA
+print(name.lower())   # amira
+print(name.title())   # Amira
+
+# 4. Check content
+print(name.isalpha())  # True (only letters)
+print("123".isdigit()) # True (only digits)
+
+# 5. Slice strings
+text = "Python"
+print(text[0:3])   # Pyt
+print(text[-1])    # n
