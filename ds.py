3480cb668b4e2c21b5ec737cf35306bed5a00422
AmiraAHady <amirasgithub@gmail.com>
2025-11-08 12:17:05 +0200

    finish ds

--- ds.py
+++ ds.py
@@ -7,3 +7,25 @@ fruits[1] = "mango"       # modify
 print(fruits)             # ['apple', 'mango', 'cherry', 'orange']
 print(fruits[0])          # access first item
 print(len(fruits))        # length
+
+
+
+colors = ("red", "green", "blue")
+print(colors[1])          # 'green'
+print(len(colors))
+# colors[1] = "yellow" ❌ (Error — tuples can’t be changed)
+
+
+numbers = {1, 2, 3, 3, 4}
+numbers.add(5)
+print(numbers)            # {1, 2, 3, 4, 5}
+numbers.remove(2)
+print(3 in numbers)       # True
+
+
+person = {"name": "Amira", "age": 25, "city": "Cairo"}
+print(person["name"])     # Amira
+person["age"] = 26        # update
+person["email"] = "amira@example.com"  # add new
+print(person.keys())      # dict_keys(['name', 'age', 'city', 'email'])
+
