896f41a21a6a6714b4f97b67aba6e9a0482db84c
AmiraAHady <amirasgithub@gmail.com>
2025-11-08 11:09:05 +0200

    deal with files

--- day_1.py
+++ day_1.py
@@ -1,6 +1,27 @@
 x=5
+y=12
 
 def printx():
     print(x)
     
-printx()    
+printx() 
+
+with open("example.txt", "w") as file:
+    file.write("Hello, world!\n")
+    file.write("This is a sample text file.\n")  
+
+
+with open("example.txt", "r") as file:
+    content = file.read()
+    print("File content:")
+    print(content)
+    
+with open("example.txt", "a") as file:
+    file.write("Adding another line.\n")
+
+# 4. Read again to see the update
+with open("example.txt", "r") as file:
+    print("Updated content:")
+    print(file.read())        
+
+ 
