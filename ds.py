bdcd0706ac2bd80606dbe624af247e0617b7687c
AmiraAHady <amirasgithub@gmail.com>
2025-11-08 12:14:04 +0200

    add ds

---
+++ ds.py
@@ -0,0 +1,9 @@
+
+
+# 🧩 1. List → ordered, changeable, allows duplicates
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")   # add
+fruits[1] = "mango"       # modify
+print(fruits)             # ['apple', 'mango', 'cherry', 'orange']
+print(fruits[0])          # access first item
+print(len(fruits))        # length
